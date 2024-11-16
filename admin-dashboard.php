7a69d83a5e8fb01b2988f4410c9427ba1d3bc301
Dhanithya Beligolla <it22085580@my.sliit.lk>
2024-11-16 23:07:57 +0530

    update admin dashboard

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -14,14 +14,13 @@ if (!isset($_SESSION['admin_id'])) {
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    
+
     <div class="container">
         <h1>Welcome, <?php echo $_SESSION['admin_username']; ?>!</h1>
         <nav>
             <ul>
                 <li><a href="view-users.php">View All Users</a></li>
-                <li><a href="view-events.php">View Events</a></li>
-                <li><a href="create-event.php">Create Event</a></li>
+                <li><a href="view-events.php">Manage Events</a></li>
                 <li><a href="view-bookings.php">View All Bookings</a></li>
                 <li><a href="php/admin-logout.php">Logout</a></li>
             </ul>
